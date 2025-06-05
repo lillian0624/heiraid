5467b113a2c8e0b9babdb19ee9b32e4ff39e01b2
NoelOsiro <noel.osiro@azubiafrica.org>
2025-06-05 22:26:30 +0300

    workflows

---
+++ backend/startup.cmd
@@ -0,0 +1,7 @@
+@echo off
+
+:: Install dependencies from requirements.txt
+pip install -r requirements.txt
+
+:: Run the FastAPI application with uvicorn
+uvicorn src.main:app --host 0.0.0.0 --port 8000 --workers 4
